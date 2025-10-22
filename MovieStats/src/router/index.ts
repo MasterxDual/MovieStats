ade980434a145026f07a07cbfe34ad237b7ddb35
Agustin Brambilla <agustinbram@gmail.com>
2025-10-22 16:08:17 -0300

    Admin Screen Frontend Ready - Falta unirlo al backend y realizar el backend tambien

--- MovieStats/src/router/index.ts
+++ MovieStats/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 // import HomeView from '@/views/HomeView.vue';
 import LoginView from '@/views/LoginView.vue';
 import RegisterView from '@/views/RegisterView.vue';
+import AdminView from '@/views/AdminView.vue';
 
 const routes = [
   // Route '/' will be the catalog view in the future
@@ -12,7 +13,8 @@ const routes = [
   // { path: '/cart', component: CartView, meta: { requiresAuth: true } },
   { path: '/', redirect: '/register' },
   { path: '/login', component: LoginView },
-  { path:'/register', name: 'Register', component: RegisterView }
+  { path:'/register', name: 'Register', component: RegisterView },
+  { path:'/admin', name: 'Admin', component: AdminView }
 ];
 
 const router = createRouter({
